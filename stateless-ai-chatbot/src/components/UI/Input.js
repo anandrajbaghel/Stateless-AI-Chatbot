1f2fcc9af302fd28a257412bb86b950ee4b20db8
Anand Baghel <100018209+anandrajbaghel@users.noreply.github.com>
2024-12-17 19:27:21 +0530

    Create Input.js

---
+++ stateless-ai-chatbot/src/components/UI/Input.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 1rem;
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+export default Input;
